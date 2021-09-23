80992ec0dcaf4329021fc9e2b23dad61d04edbd1
manhdg98 <jokermanh@jokers-Mac.local>
2021-09-23 10:17:39 -0700

    add widget for filter input

--- lib/src/feature/presentation/tabs/explore_tab.dart
+++ lib/src/feature/presentation/tabs/explore_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:manfood/src/colors/colors.dart';
 
 class ExploreTab extends StatelessWidget {
   const ExploreTab({Key? key}) : super(key: key);
@@ -23,6 +24,43 @@ class ExploreTab extends StatelessWidget {
 
 Widget _topBar(BuildContext context) {
   return Row(
+    children: [
+      Container(
+        width: 310,
+        padding: EdgeInsets.all(10.0),
+        margin: EdgeInsets.only(left: 16),
+        decoration: BoxDecoration(
+          border: Border.all(color: Color.fromRGBO(234, 236, 239, 1.0)),
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        child: Row(
+          children: [
+            Icon(Icons.search, size: 20.0, color:gris,),
+            Container(
+              margin: EdgeInsets.only(left: 8),
+              child: Text('Search', style: TextStyle(
+                color: gris,
+                fontSize: 17.0
+              )),
+            )
+          ],
+        ),
+      ),
+      Container(
+        width: 45.0,
+        height: 45.0,
+        margin: EdgeInsets.only(left: 30),
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(209, 209, 214, 1.0),
+          borderRadius: BorderRadius.circular(30)
+        ),
+        child: IconButton(
+          icon: Icon(Icons.filter_list, size: 30, color: Colors.white),
+          onPressed: () {
 
+          },
+        ),
+      )
+    ],
   );
 }
